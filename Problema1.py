c9f572e750e19ce2dbcd2395a1945f82d17422f9
SofyParada <96263962+SofyParada@users.noreply.github.com>
2024-05-09 14:17:24 -0400

    Update Problema1.py

--- Problema1.py
+++ Problema1.py
@@ -1,59 +1,51 @@
-def superposicion(A, n):
-    #n = len(A)
-    if n == 1:
-        return 0
-    else:
-        # Dividir el conjunto A en dos mitades
-        A1 = A[:n//2]  # A1 mitad izquierda de A
-        A2 = A[n//2:]  # A2 mitad derecha de A
+
+def min_suma_gaps_cuadrado(L, n, ladrillos): #en construcción 
+    suma = 0
+    pared = []
+    i = 0
+    while i <= n:
+        if suma > L:
+            pared.append(suma - int(ladrillos[i-1]))#se le elimina el ultimo que se le sumo ya que no puede ser mayor a L
+            suma = 0
+            suma += int(ladrillos[i-1])
+
         
-        # Resolver recursivamente el problema para las dos mitades
-        maxA = superposicion(A1, len(A1))
-        maxB = superposicion(A2, len(A2))    
+        elif suma == L and i < n:
+            pared.append(suma)
+            suma = 0
+            suma += int(ladrillos[i])
+            i += 1
 
-        i = 0 
-        j = 0 
-        max_final = 0
+        elif i == n:
+            pared.append(int(ladrillos[i-1]))
+            i+= 1
 
-        #Hay que comparar los intervalos y ver el valor de la intersección
+        else:
+            suma += int(ladrillos[i])
+            i += 1
 
-        while i < len(A1) and j < len(A2):  #Recorre largo de mitades
-
-            if A1[i][1] >= A2[j][0]: #Si el final de A1 es mayor o igual al inicio de A2, hay intersección
-                inters = min(A1[i][1], A2[j][1]) - A2[j][0] + 1  #Elige el menor de los dos finales y le resta el inicio del otro, mas 1
-                if inters > max_final:
-                    max_final = inters
-                j += 1  
-                if j == len(A2) and i < len(A1):
-                    j = 0
-                    i += 1
-            else:
-                i += 1
-        
-        # Se toman maximos de las recursividades y el procedimiento actual
-        return max(maxA, maxB, max_final)
+ 
+    return  pared
 
 
 
+x = input() #"6 9"
+L, n= map(int, x.strip().split(" "))
 
-   
+print(L, n)
 
-x = input()
 
 while x != "": 
-    i = input()
-    linea = i.split(" ")
-    conjunto = []
-    y = 0
-    while y < 2*int(x) :   
-            conjunto.append((int(linea[y]), int(linea[y + 1])))
-            y += 2
-            
-    conjunto = sorted(conjunto, key=lambda x: x[0])
-    #print(conjunto)
-    print(superposicion(conjunto , int(x)))
+    i = input() 
+    ladrillos = i.strip().split(" ")
+
+    resultado = min_suma_gaps_cuadrado(L, n, ladrillos)
+    print(resultado)
+    
     try:
         x= input()
+        L, n = map(int, x.strip().split(" "))
+        print(L,n)
     except EOFError as er:
         x = ""
 
